9cbe2832ac5d87c44ec016d5700402fdaa30efd6
phdgit <phdgnr@gmail.com>
2019-03-21 22:25:27 +0100

    format code

--- wp-content/plugins/recaptcha/public/js/wqrecaptcha-public.js
+++ wp-content/plugins/recaptcha/public/js/wqrecaptcha-public.js
@@ -1,7 +1,7 @@
-(function($) {
+(function ($) {
 	'use strict';
 
-	$(document).ready(function() {
+	$(document).ready(function () {
 		/**
 		 * disable submit buttons / input
 		 */
@@ -10,8 +10,10 @@
 		var sitekey = WQverifcaptcha_ajax.sitekey;
 		var URL = WQverifcaptcha_ajax.url;
 		// site key preprod
-		grecaptcha.ready(function() {
-			grecaptcha.execute(sitekey, { action: 'contact' }).then(function(token) {
+		grecaptcha.ready(function () {
+			grecaptcha.execute(sitekey, {
+				action: 'contact'
+			}).then(function (token) {
 				// Verify the token on the server.
 				$.ajax({
 					url: URL,
@@ -21,11 +23,11 @@
 						token: token
 						// queryvars: verifcaptcha_ajax.queryvars
 					},
-					success: function(res) {
+					success: function (res) {
 						if (res == 'success') $(':submit').prop('disabled', false);
 						// if(res=='error') ...;
 					},
-					error: function(err) {
+					error: function (err) {
 						console.log('error', err);
 					}
 				});
